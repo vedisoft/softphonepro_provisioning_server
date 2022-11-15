7dc820c00e23fbd2aaabf313b6f7a34ac35fe8e0
vedisoft <info@vedisoft.ru>
2022-11-15 10:11:37 +0500

    update link and templates

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ return array(
     'logsDir' => __DIR__ . '/logs/',
     'configsDir' => __DIR__ . '/configs/',
     'templatesDir' => __DIR__ . '/templates/',
-    'valid_version' => '4.2.1.0',
-    'update_link' => 'http://office.vedisoft.ru/files/Softphone_4.2.1.0.exe',
+    'valid_version' => '5.0.1.0',
+    'update_link' => 'https://softphone.pro/download/SoftphonePro_5_0_1.exe',
 );
